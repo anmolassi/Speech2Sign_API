04625b95e99fc4db84e065a1f871d376f9049349
anmolassi <79657095+anmolassi@users.noreply.github.com>
2023-12-19 10:54:11 +0530

    backToAPI

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ app.use('/uploads',express.static(__dirname + '/uploads'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.set('views','./views');
-// app.use('/',require('./routes'));//it will move to routes/index.js for furthur 
+app.use('/',require('./routes'));//it will move to routes/index.js for furthur 
 var ip = require('ip');
 var geoip = require('geoip-lite');
-app.get('/',function(req,res){
-    // var clientIp = requestIp.getClientIp(req)
-    // var clientIp=ip.address();
-    var clientIp=req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    clientIp=clientIp.split(',');
-    res.send(`Your IP Address is ${clientIp[0]}.`);
-    console.log(geoip.lookup(clientIp[0]));
-})
+// app.get('/',function(req,res){
+//     // var clientIp = requestIp.getClientIp(req)
+//     // var clientIp=ip.address();
+//     var clientIp=req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//     clientIp=clientIp.split(',');
+//     res.send(`Your IP Address is ${clientIp[0]}.`);
+//     console.log(geoip.lookup(clientIp[0]));
+// })
 app.use(express.urlencoded({extended:false}));
 app.listen(port,function(err){
     if(err)
